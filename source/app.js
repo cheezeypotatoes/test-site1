b2de060e49195d6152242485558873871e520229
Curt Rowland <curt8769@gmail.com>
2017-11-15 11:37:32 -0800

    change img hilight color and add about click events

--- source/app.js
+++ source/app.js
@@ -1,22 +1,36 @@
-console.log('Script Loaded');
-let nodeList = document.querySelectorAll('.images-container img')
-
-// console.log(nodeList);
-
-for(let i = 0; i < nodeList.length; i++) {
-  console.log(nodeList[i]);
-  console.log(i);
+console.log('Script Loaded')
+let img = document.querySelectorAll('.images-container img')
+let aboutMsg = document.querySelectorAll('.about-container')
+let isBold = false
 
 
-  nodeList[i].addEventListener('mouseenter',function (e) {
-    // console.log('mouse entered div, effects will take place');
-    nodeList[i].classList.add('hilighted-image')
+for(let i = 0; i < img.length; i++) {
+
+  img[i].addEventListener('mouseenter',function (e) {
+    // console.log('mouse entered div, effects will take place')
+    img[i].classList.add('hilighted-image')
   })
 
-  nodeList[i].addEventListener('mouseout',function (e) {
-    // console.log('mouse exited div, effects will be removed');
-    nodeList[i].classList.remove("hilighted-image");
+  img[i].addEventListener('mouseout',function (e) {
+    // console.log('mouse exited div, effects will be removed')
+    img[i].classList.remove('hilighted-image')
   })
+}
 
+for(let i2 = 0; i2 < aboutMsg.length; i2++) {
+  console.log(aboutMsg[i2])
+  aboutMsg[i2].addEventListener('click', function (e) {
 
+    if(isBold) {
+      //code to remove class
+      aboutMsg[i2].classList.remove('bold')
+      isBold = !isBold
+      // console.log(aboutMsg[i2] + ' should be false here, real value is ' + isBold)
+    } else {
+      // code to add class
+      aboutMsg[i2].classList.add('bold')
+      isBold = !isBold
+      // console.log(aboutMsg[i2] + ' should be true here, real value is ' + isBold)
+    }
+  })
 }
