5c821ad575bd9fd03319c889941d7d5949ce1195
Curt Rowland <curt8769@gmail.com>
2017-11-16 13:59:46 -0800

    Rework gallery add click functionality

--- source/gallery.js
+++ source/gallery.js
@@ -15,6 +15,10 @@ for(let i = 0; i < img.length; i++) {
     // console.log('mouse exited div, effects will be removed')
     img[i].classList.remove('hilighted-image')
   })
+
+  img[i].addEventListener('click', function(e) {
+    displayImage(this.src, this.alt)
+  })
 }
 
 for(let i2 = 0; i2 < aboutMsg.length; i2++) {
@@ -35,4 +39,18 @@ for(let i2 = 0; i2 < aboutMsg.length; i2++) {
       // console.log(aboutMsg[i2] + ' should be true here, real value is ' + isBold)
     }
   })
+}
+
+function displayImage(image, alt) {
+
+  //while loop checks for previous img and deletes it before dislplaying next img clicked.
+  var displayImage = document.querySelector(".display-image");
+    while (displayImage.hasChildNodes()) {
+        displayImage.removeChild(displayImage.firstChild);
+    }
+
+  //creates img in div id display of the img clicked
+  var newImage = document.createElement("IMG");
+  newImage.setAttribute("src", image);
+  document.querySelector(".display-image").appendChild(newImage);
 }
